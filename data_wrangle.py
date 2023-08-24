ea0c451c5683943367bc86a3b39cf9d54b5fe200
Ricky Macharm, MScFE <33702921+theAfricanQuant@users.noreply.github.com>
2023-08-24 11:48:29 +0200

    initial commit

---
+++ data_wrangle.py
@@ -0,0 +1,47 @@
+
+import pandas as pd
+import datetime as dt
+
+
+def show_nulls(df):
+    return (df[df
+            .isna()
+            .any(axis=1)]
+           )
+    
+def total_nulls(df):
+    return (df
+         .isna()
+         .sum()
+         .sum()
+        )
+
+def get_data(url):
+    return (
+        pd.read_csv(url)
+    )
+
+def mean_vals(df, idx1, idx2, col):
+    return (
+        (df.iloc[idx1][col] + 
+        df.iloc[idx2][col])/2
+    )
+
+def prep_data(data):
+    return (data
+            .assign(windspeed = data["windspeed"]
+                    .fillna((data["windspeed"]
+                             .median())),
+                    hum = (data['hum']
+                   .fillna(data.groupby('season')['hum']
+                           .transform('median'))),
+                    temp = (data['temp']
+                            .fillna(mean_vals(data, 700, 702, 'temp'))),
+                    atemp = (data['atemp']
+                            .fillna(mean_vals(data, 700, 702, 'atemp'))),
+                    dteday = pd.to_datetime(data['dteday']),
+                    mnth = lambda x: x['dteday'].dt.month,
+                    yr = data['yr'].ffill()
+                   )
+            .drop('dteday', axis=1)
+           )
